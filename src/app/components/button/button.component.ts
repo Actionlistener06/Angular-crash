ff166183f4d27873ee970b7b82566b2e6447e691
Mustafa Serhat Demirtaş <93614252+Actionlistener06@users.noreply.github.com>
2023-06-27 00:13:42 +0300

    http requests methods added

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component,EventEmitter,Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+  @Output() btnClick = new EventEmitter();
+  constructor() { }
+
+  onClick() {
+    console.log('click');
+    this.btnClick.emit();
+    }
+
+}
